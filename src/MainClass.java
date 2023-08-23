d6857d64c3457ef132a5ca4ce9f8c745468659f9
daniel.dacruz@students.fhnw.ch <Hustler2019$>
2023-08-23 09:57:05 +0200

    setup in InteliJ

---
+++ src/MainClass.java
@@ -0,0 +1,8 @@
+import amaze.Amaze;
+import processing.core.PApplet;
+
+public class MainClass extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main(Amaze.class, args);
+    }
+}
