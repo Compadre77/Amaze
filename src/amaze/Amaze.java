7a7ecbaaaa89b958385bc0eef38c572521302377
larissa.martinssequeira <larissa.martinssequeira@students.fhnw.ch>
2023-08-23 21:32:55 +0200

    fix player & add win message

--- src/amaze/Amaze.java
+++ src/amaze/Amaze.java
@@ -2,7 +2,6 @@ package amaze;
 
 import processing.core.PApplet;
 import processing.core.PFont;
-import processing.core.PGraphics;
 import processing.core.PImage;
 
 public class Amaze extends PApplet {
@@ -13,6 +12,7 @@ public class Amaze extends PApplet {
 
     PImage playerImg;
     PFont f;
+    PFont winMessage;
 
     int playerX, playerY;
     int speed = 30;
@@ -28,11 +28,9 @@ public class Amaze extends PApplet {
     boolean startButtonPressed = false;
     boolean resetButtonPressed = false;
 
-
-
-
-    PGraphics spotlight;
-
+    boolean gameCompleted = false;
+    int exitX = 19 * 30;
+    int exitY = 1 * 30;
 
     int[][] maze = new int[][] {
             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
@@ -66,11 +64,10 @@ public class Amaze extends PApplet {
     public void setup() {
         playerImg = loadImage("./ressources/knight.png");
         f = createFont("Arial", 16, true);
+        winMessage = createFont("./ressources/Gameplay.ttf", 28);
         playerImg.resize(playerSize, 0);
         playerX = 1;
         playerY = 30;
-
-        spotlight = createGraphics(width, height);
 
         extracted();
         System.out.println(maze[0][0]);
@@ -89,64 +86,112 @@ public class Amaze extends PApplet {
     }
 
     public void draw() {
-        background(255);
+        if (gameCompleted) {
+            background(0);
+            textFont(winMessage);
+            textAlign(CENTER, CENTER);
+            fill(255, 215, 0);
+            text("You have found the exit!\nCongratulations!", width / 2, height / 2);
 
-        for (int i = 0; i < maze.length; i++) {
-            for (int j = 0; j < maze[i].length; j++) {
-                if (maze[i][j]==1) {
-                    fill(0); // Draw boundaries in black
-                } else {
-                    fill(255); // Draw open spaces in white
-                }
-                noStroke();
-                rect(j * cellSize, i * cellSize, cellSize, cellSize);
-            }
-        }
-
-        fill(255, 240);
-        rect(playerX+5,playerY+5,playerSize-15,playerSize-15);
-        image(playerImg, playerX, playerY);
-        if (start) {
-            textFont(f, 16);
+            textFont(f);
+            textAlign(LEFT, BASELINE);
             fill(0);
-            text("", 190, 650);
-        } else {
-            textFont(f, 16);
-            fill(0);
-            text("Um zu spielen, klicke auf \"Start\"", 190, 650);
-        }
 
-        if(reset){
-            playerX = 1;
-            playerY = 30;
-        }
-
-        //Start Button
-        if (startButtonPressed) {
-            fill(34, 139, 34, 100);
-        } else {
+            // Start Button
             fill(34, 139, 34);
-        }
-        rect(150, 700, 100, 40, 10, 10, 10, 10);
-        fill(0, 0, 0);
-        text("Start", 180, 725);
+            rect(150, 700, 100, 40, 10, 10, 10, 10);
+            fill(0, 0, 0);
+            text("Start", 180, 725);
 
-
-        //End Button
-        if (resetButtonPressed) {
-            fill(178, 34, 34, 100);
-        } else {
+            // Reset Button
             fill(178, 34, 34);
+            rect(350, 700, 100, 40, 10, 10, 10, 10);
+            fill(0, 0, 0);
+            text("Reset", 380, 725);
+
+        } else {
+            background(255);
+
+            for (int i = 0; i < maze.length; i++) {
+                for (int j = 0; j < maze[i].length; j++) {
+                    if (maze[i][j]==1) {
+                        fill(0); // Draw boundaries in black
+                    } else {
+                        fill(255); // Draw open spaces in white
+                    }
+                    noStroke();
+                    rect(j * cellSize, i * cellSize, cellSize, cellSize);
+                }
+            }
+
+            fill(255, 240);
+            rect(playerX+5,playerY+5,playerSize-15,playerSize-15);
+            image(playerImg, playerX, playerY);
+
+
+            float rectX = playerX - gridSize, rectY = playerY - gridSize-200, rectWidth = 2*width, rectHeight = 2*height;
+            drawRadialGradient(rectX, rectY, rectWidth, rectHeight, color(0, 0, 0));
+
+            if (start) {
+                textFont(f, 16);
+                fill(0);
+                text("", 190, 650);
+            } else {
+                textFont(f, 16);
+                fill(0);
+                text("Um zu spielen, klicke auf \"Start\"", 190, 650);
+            }
+
+            if(reset){
+                playerX = 1;
+                playerY = 30;
+                gameCompleted = false;
+                reset = false;
+            }
+
+            //Start Button
+            if (startButtonPressed) {
+                fill(34, 139, 34, 100);
+            } else {
+                fill(34, 139, 34);
+            }
+            rect(150, 700, 100, 40, 10, 10, 10, 10);
+            fill(0, 0, 0);
+            text("Start", 180, 725);
+
+
+            //End Button
+            if (resetButtonPressed) {
+                fill(178, 34, 34, 100);
+            } else {
+                fill(178, 34, 34);
+            }
+            rect(350, 700, 100, 40, 10, 10, 10, 10);
+            fill(0, 0, 0);
+            text("Reset", 380, 725);
+
+            if(playerX == exitX && playerY == exitY) {
+                gameCompleted = true;
+            }
+
+
+
+
+
+
+
+            playerX = constrain(playerX, 0, width - playerImg.width);
+            playerY = constrain(playerY, 0, height - playerImg.height);
+
+
+            //fill(0);
+            //rect(0, 0, width, height);
+            //fill(255);
+            //textSize(32);
+            //text("You have found the exit!\nCongratulations!", width / 2 -200, height / 2);
         }
-        rect(350, 700, 100, 40, 10, 10, 10, 10);
-        fill(0, 0, 0);
-        text("Reset", 380, 725);
 
-        float rectX = playerX - gridSize, rectY = playerY - gridSize-200, rectWidth = 2*width, rectHeight = 2*height;
-        drawRadialGradient(rectX, rectY, rectWidth, rectHeight, color(0, 0, 0));
 
-        playerX = constrain(playerX, 0, width - playerImg.width);
-        playerY = constrain(playerY, 0, height - playerImg.height);
     }
 
     void drawRadialGradient(float x, float y, float w, float h, int c) {
